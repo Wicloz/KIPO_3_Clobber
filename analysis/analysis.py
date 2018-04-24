13a7720dc8637e4137a5a7ec86a93254302fb8c1
Wilco de Boer <wfh@wilcodeboer.me>
2018-04-24 15:22:54 +0200

    andere spelers beter

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -6,27 +6,28 @@ import matplotlib as plt
 from multiprocessing import Pool
 
 # parameters
-ourPlayer = 0
 boardSize = ['6', '6']
 playingTypes = ['minimax', 'alphabeta', 'avgmax']
 n = 20
+players = ['0', '1']
 fileLocation = '../cmake-build-debug/KIPO_3_Clobber'
 threadCount = 8
 
 # functions
 def runAnalysis(args):
-    output = sp.check_output([fileLocation, boardSize[0], boardSize[1], args[0], args[1]], universal_newlines=True)
+    output = sp.check_output([fileLocation, boardSize[0], boardSize[1], args[0], args[1], args[2]], universal_newlines=True)
     print('#', end='', flush=True)
     return {
         'seed': args[0],
         'type': args[1],
-        'result': int((output.splitlines()[-2]).split(' ')[-1]),
+        'player': args[2],
+        'result': (output.splitlines()[-2]).split(' ')[-1],
     }
 
 if __name__ == '__main__':
     # n random seeds
     seeds = [str(x) for x in sample(range(0, 2147483647), n)]
-    inputs = [(seed, type) for seed in seeds for type in playingTypes]
+    inputs = [(seed, type, player) for seed in seeds for type in playingTypes for player in players]
 
     # results storage
     results = [[0, 0] for x in range(0, len(playingTypes))]
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     print('Analysing - ', end='', flush=True)
 
     for output in pool.map(runAnalysis, inputs):
-        if output['result'] == ourPlayer:
+        if output['result'] == output['player']:
             results[playingTypes.index(output['type'])][0] += 1
         else:
             results[playingTypes.index(output['type'])][1] += 1
@@ -47,6 +48,7 @@ if __name__ == '__main__':
     df = pd.DataFrame(results, index=playingTypes, columns=['wincount', 'losecount'])
     print(df)
 
+    n = n * len(players)
     ystep = int(math.ceil(n / 10))
     plot = df.plot.bar(stacked=True, yticks=range(0, n + ystep, ystep), title='Win-Lose ratio on a ' + str(boardSize[0]) + 'x' + str(boardSize[1]) + ' board with n=' + str(n))
     plot.set_ylabel('Games Played')
